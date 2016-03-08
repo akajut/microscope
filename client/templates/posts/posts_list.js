35b3feddaa8995bf54a91eb9a647c37873e533df
Justin McMinn <akajut@gmail.com>
2016-03-08 14:08:59 -0600

    3-1 Added basic posts list template and static data.

---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,17 @@
+var postsData = [
+  {
+    title: 'Tintroducting Telescope',
+    url: 'http://sachagrief.com/introducting-telescope/'
+  },
+  {
+    title: 'Meteor',
+    url: 'http://meteor.com'
+  },
+  {
+    title: 'The Meteor Book',
+    utl: 'http:themeteorbook.com'
+  }
+];
+Template.postsList.helpers({
+  posts: postsData
+});
