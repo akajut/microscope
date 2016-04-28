4e4bf78f985a5bebaaadb85ee53fc77f21a0b828
Justin McMinn <akajut@gmail.com>
2016-04-27 21:33:23 -0500

    Finishing user page logic. Deleted userPosts from publications

--- server/publications.js
+++ server/publications.js
@@ -2,11 +2,6 @@ Meteor.publish('posts', function(options) {
   return Posts.find({}, options);
 });
 
-Meteor.publish('userPosts', function(userId) {
-  check(userId, String);
-  return Posts.find(userId);
-});
-
 Meteor.publish('singlePost', function(id) {
   check(id, String);
   return Posts.find(id);
